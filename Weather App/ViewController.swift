3f010fe48f21678851a2e600c8ca0b3073f92129
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-10-16 01:05:16 -0300

    created the headerView

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var customView: UIView = {
+    private lazy var backgroundView: UIImageView = {
+        let ImageView = UIImageView(frame: .zero)
+        ImageView.image = UIImage (named: "background")
+        ImageView.contentMode = .scaleAspectFill
+        ImageView.translatesAutoresizingMaskIntoConstraints = false
+        return ImageView
+    }()
+    
+    private lazy var headerView: UIView = {
         let view = UIView(frame: .zero)
-        view.backgroundColor = UIColor.blue
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20
         return view
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Sao Paulo"
+        label.textAlignment = .center
+        return label
     }()
 
     override func viewDidLoad() {
@@ -28,15 +46,31 @@ class ViewController: UIViewController {
     }
     
     private func setHierarchy() {
-        view.addSubview(customView)
+        view.addSubview(backgroundView)
+        view.addSubview(headerView)
+        
+        headerView.addSubview(cityLabel)
     }
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
-            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
-            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 35),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            headerView.heightAnchor.constraint(equalToConstant: 169)
+        ])
+        
+        NSLayoutConstraint.activate([
+            cityLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+            cityLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
+            cityLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15)
         ])
     }
 }
